a3b71f11fbe867ff813426215a9f2c3bbefdb073
idist_imac <>
2023-01-21 03:21:49 +0900

    jodit build success

--- examples/components/Form.js
+++ examples/components/Form.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-// import JoditEditor from '../../src/';
-import JoditEditor from '../../build/jodit-react';
-import './Form.css';
+import React, { useState } from "react";
+import JoditEditor from "../../src/";
+// import JoditEditor from "../../build/jodit-react";
+import "./Form.css";
 
 /**
  * @param {Jodit} jodit
  */
 const Form = () => {
-	// States
-	const [config] = useState({
-		toolbarStyle: 'top',
-		minHeight: '500px',
-		uploaderApi: 'https://dev.server.superclubs.io/api/v1/media/'
-	});
-	const [textAreaValue] = useState('');
+  // States
+  const [config] = useState({
+    toolbarStyle: "top",
+    minHeight: "500px",
+    uploaderApi: "https://dev.server.superclubs.io/api/v1/media/",
+  });
+  const [textAreaValue] = useState("");
 
-	return (
-		<div>
-			<JoditEditor config={config} />
-			{textAreaValue}
-		</div>
-	);
+  return (
+    <div>
+      <JoditEditor config={config} />
+      {textAreaValue}
+    </div>
+  );
 };
 
 export default Form;
